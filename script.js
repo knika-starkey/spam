0e1844f44113ef0360bc6aca6878415635d957a5
knika-starkey <knika787@gmail.com>
2021-11-26 18:44:26 +0200

    spam change to ***

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function spamChecker(str) {
   let checkline = str.toLowerCase();
-  if (checkline.indexOf("spam") >= 0) {
-    alert("true");
-    return true;
-  } else {
-    alert("false");
-    return false;
+  let arrold = str.split(" ");
+  let arrnew = checkline.split(" ");
+  let newline = "";
+  for (let i = 0; i < arrold.length; i++) {
+    if (arrnew[i] == "spam") arrold[i] = "***";
+    newline += arrold[i] + " ";
   }
+  return newline;
 }
-
 let line = prompt("Введите строку");
-spamChecker(line);
+alert(spamChecker(line));
